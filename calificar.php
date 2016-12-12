7466d97c0ac6492e6289248ae4d29e28051029b0
cognatus <diegojmartinezzm@gmail.com>
2016-12-11 21:00:52 -0600

    furula, ahora con php

--- calificar.php
+++ calificar.php
@@ -2,5 +2,35 @@
 	
 	echo "Tu calficacion es:";
 	echo $_POST['calficacion'];
+	
+	session_start();
+
+	include 'conexion.php';
+
+	$base = conecta();
+	if ($_SESSION["nivel"] == 1) {
+		$query = "UPDATE alumnos SET calif1=$_POST['calficacion'], nivel=2 WHERE boleta=$_SESSION["boleta"];";
+	}else if ($_SESSION["nivel"] == 2) {
+		$query = "UPDATE alumnos SET calif2=$_POST['calficacion'], nivel=3 WHERE boleta=$_SESSION["boleta"];";
+	}else if ($_SESSION["nivel"] == 3) {
+		$query = "UPDATE alumnos SET calif3=$_POST['calficacion'], nivel=0 WHERE boleta=$_SESSION["boleta"];";
+	}else{
+		echo "Tu calificacion no puede ser guardada";
+		sleep(10);
+		session_start();
+
+		session_unset(); 
+
+		session_destroy(); 
+
+		header("Location: /DominoCanvas");
+	}
+
+	if(mysqli_query($base, $query)){
+		echo "exito guardando calificacion";
+	}else{
+		echo 'Error: ' . mysqli_error($base);
+		echo $query;
+	}
 
  ?>
